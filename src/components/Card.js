c6a5945fbba2736109ce0227f1c26e8320b8cc9f
Jefferson Morales <134145688+MoralesJefferson@users.noreply.github.com>
2023-06-14 12:39:28 -0300

    inicio-exemplo

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+const Card = () => {
+
+    return (
+        <div className='cardContainer'>
+            <p>Meu primeiro componente em React</p>
+        </div>    
+    );
+}
+
+export default Card;
